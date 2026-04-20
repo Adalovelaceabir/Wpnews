3c74d51896185cbe2459a6db2064aed7869c98f4
Ada Lovelace Abir <131441525+Adalovelaceabir@users.noreply.github.com>
2026-04-20 10:47:27 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main><!-- #content -->
+
+<footer id="colophon" class="site-footer">
+    <div class="container">
+        <div class="site-info">
+            &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
